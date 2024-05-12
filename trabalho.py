1cad71ba84fb2f88a50473e026fb31230e659fcb
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-05-12 16:19:39 -0300

    first commit

---
+++ trabalho.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+
+def create_tables(): #doug 
+    pass
+
+def obter_sessoes(): #joao 
+    pass
+
+def adicionar_sessao(): #lohan 
+    pass
+
+def adicionar_produto(): #paulo
+    pass
+
+def mostrar_estoque(): #paulo
+    pass
+
+def remover_sessao(): #lucas
+    pass
+
+def main():  #rafael interface do programa
+    pass
+
+if __name__ == "__main__":
+    main()
