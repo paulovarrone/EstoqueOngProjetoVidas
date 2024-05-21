6c5648d74758f1ad795d34ee95524a89421836dc
Lohan Flores <lohan.conrado.flores@gmail.com>
2024-05-20 23:16:28 -0300

    criar tabelas

--- trabalho.py
+++ trabalho.py
@@ -2,8 +2,28 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 
-def create_tables(): #doug 
-    pass
+def create_tables(): #Lohan
+    db_conn = sqlite3.connect("estoque.db")
+    db_cursor = db_conn.cursor()
+    
+    db_cursor.execute('''CREATE TABLE IF NOT EXISTS sessoes (
+                                id INTEGER PRIMARY KEY,
+                                nome TEXT NOT NULL)''')
+    
+    db_cursor.execute('''CREATE TABLE IF NOT EXISTS produtos (
+                                id INTEGER PRIMARY KEY,
+                                nome TEXT NOT NULL,
+                                sessao_id INTEGER NOT NULL,
+                                FOREIGN KEY(sessao_id) REFERENCES sessoes(id))''')
+    
+    db_cursor.execute('''CREATE TABLE IF NOT EXISTS estoque (
+                                id INTEGER PRIMARY KEY,
+                                produto_id INTEGER NOT NULL,
+                                quantidade INTEGER NOT NULL,
+                                FOREIGN KEY(produto_id) REFERENCES produtos(id))''')
+    
+    db_conn.commit()
+    db_conn.close()
 
 def obter_sessoes(): #joao 
     pass
@@ -28,7 +48,7 @@ def main():  #rafael interface do programa
     root['bg'] = '#0b5884'
     root.resizable(0, 0)
 
-    #create_tables()
+    create_tables()
 
     label_produto = tk.Label(root, text="Produto:", font=("Arial", 15), bg='white')
     label_produto.grid(row=0, column=0, padx=20, pady=20)
@@ -48,7 +68,7 @@ def main():  #rafael interface do programa
     entry_sessao = tk.Entry(root, font=("Arial", 15))
     entry_sessao.grid(row=2, column=1, padx=20, pady=20)
 
-    btn_adicionar = tk.Button(root, text="Adicionar ou Remover Produto", font=("Arial", 15), bg='white', command)
+    btn_adicionar = tk.Button(root, text="Adicionar ou Remover Produto", font=("Arial", 15), bg='white', command=)
     btn_adicionar.grid(row=3, column=0, columnspan=2, padx=20, pady=20)
 
     btn_adicionar_sessao = tk.Button(root, text="Adicionar Sessão", font=("Arial", 15), bg='white', command=)
